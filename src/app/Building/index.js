aa0d725f8de4731231389d453f70a06b26d52603
Naomi Hauret <naomi@troopers.email>
2018-11-27 16:25:58 +0100

    :wrench: added deploy script

--- src/app/Building/index.js
+++
@@ -1,6 +0,0 @@
-import { MeshBuilder } from 'babylonjs'
-
-export const addBuilding = (scene, name, height, width, depth, y) => {
-  let building = MeshBuilder.CreateBox("building", { height: height, width: width, depth: depth }, scene)
-  building.position.y = y
-}
