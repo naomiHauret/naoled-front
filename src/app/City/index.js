60a1ea52f8f69ddb62a00dcc241f7990524160cc
Clément Lemoine <clementlemoine.cl@gmail.com>
2018-11-28 15:04:03 +0100

    ✨postprocessing effects

--- src/app/City/index.js
+++ src/app/City/index.js
@@ -1,9 +1,11 @@
 import { MeshBuilder, Mesh, Vector3, Color3, StandardMaterial } from 'babylonjs'
 
 export const createCity = (scene) => {
-  // Material
-  const buildingMaterial = new StandardMaterial("buildingMaterial", scene)
-  buildingMaterial.diffuseColor = new Color3(1, 1, 1)
+
+  const buildingMaterial = new BABYLON.PBRMetallicRoughnessMaterial("pbr", scene);
+  buildingMaterial.baseColor = new BABYLON.Color3(1.000, 1.000, 1.000);
+  buildingMaterial.metallic = 0;
+  buildingMaterial.roughness = 1.0;
 
   // Group
   const cityGroup = Mesh.CreateBox("cityGroup", 1, scene)
@@ -14,63 +16,77 @@ export const createCity = (scene) => {
   building1.parent = borroughGroup
   building1.position.y = 15
   building1.material = buildingMaterial
+  building1.receiveShadows = true;
 
   const building2 = MeshBuilder.CreateBox("building2", { height: 22, width: 5, depth: 5 }, scene)
   building2.parent = borroughGroup
   building2.position = new Vector3(8, 10, -5)
   building2.material = buildingMaterial
+  building2.receiveShadows = true;
 
   const building3 = MeshBuilder.CreateBox("building3", { height: 10, width: 4, depth: 4 }, scene)
   building3.parent = borroughGroup
   building3.position = new Vector3(0, 4.5, -15)
   building3.material = buildingMaterial
+  building3.receiveShadows = true;
 
   const building4 = MeshBuilder.CreateBox("building4", { height: 5, width: 3, depth: 3 }, scene)
   building4.parent = borroughGroup
   building4.position = new Vector3(8, 0, -10)
   building4.material = buildingMaterial
+  building4.receiveShadows = true;
 
   const building5 = MeshBuilder.CreateBox("building5", { height: 18, width: 4, depth: 4 }, scene)
   building5.parent = borroughGroup
   building5.position = new Vector3(-10, 9, -15)
   building5.material = buildingMaterial
+  building5.receiveShadows = true;
 
   const building6 = MeshBuilder.CreateBox("building6", { height: 45, width: 12, depth: 12 }, scene)
   building6.parent = borroughGroup
   building6.position = new Vector3(-10, 20, 12)
   building6.material = buildingMaterial
+  building6.receiveShadows = true;
 
   const building7 = MeshBuilder.CreateBox("building7", { height: 50, width: 14, depth: 14 }, scene)
   building7.parent = borroughGroup
   building7.position = new Vector3(10, 20, 12)
   building7.material = buildingMaterial
+  building7.receiveShadows = true;
 
   const building8 = MeshBuilder.CreateBox("building8", { height: 1.5, width: 4, depth: 8 }, scene)
   building8.parent = borroughGroup
   building8.position = new Vector3(10, 1, -15)
   building8.material = buildingMaterial
+  building8.receiveShadows = true;
 
   const building9 = MeshBuilder.CreateBox("building9", { height: 5, width: 4, depth: 8 }, scene)
   building9.parent = borroughGroup
   building9.position = new Vector3(-15, 1, -5)
   building9.material = buildingMaterial
+  building9.receiveShadows = true;
 
   const building10 = MeshBuilder.CreateBox("building10", { height: 10, width: 2, depth: 8 }, scene)
   building10.parent = borroughGroup
   building10.position = new Vector3(-15, 1, -5)
   building10.material = buildingMaterial
+  building10.receiveShadows = true;
 
   const building11 = MeshBuilder.CreateBox("building11", { height: 12, width: 7, depth: 12 }, scene)
   building11.parent = borroughGroup
   building11.position = new Vector3(20, 2, 5)
   building11.material = buildingMaterial
+  building11.receiveShadows = true;
 
 
   const subground = MeshBuilder.CreateBox("subground", { height: 0.5, width: 25, depth: 28 }, scene)
   subground.parent = cityGroup
   subground.position = new Vector3(0, -5.5, 0)
   subground.material = buildingMaterial
+  subground.receiveShadows = true;
 
+  borroughGroup.parent = cityGroup
+  borroughGroup.position = new Vector3(0, -5.8, 0)
 
   return cityGroup
 }
--- src/app/Scene/index.js
+++ src/app/Scene/index.js
@@ -1,20 +1,26 @@
 
-import { Scene, Color3, FreeCamera, Vector3, Color4, MeshBuilder, Mesh, HemisphericLight, PointLight, StandardMaterial } from 'babylonjs'
+import { Scene, Color3, FreeCamera, Vector3, Color4, MeshBuilder, Mesh, HemisphericLight, PointLight, StandardMaterial, ShadowGenerator } from 'babylonjs'
 import { createCity } from 'app/City'
 import { createCrust, addCrustSlice } from 'app/Crust'
 
 export const createScene = (engine, canvas, size) => {
     const scene = new Scene(engine)
-    scene.clearColor = new Color4(1, 0, 0, 0)
+    scene.clearColor = new Color4(0, 0, 0, 0)
 
 
     // Camera
-    const camera = new FreeCamera("camera", new Vector3(-95, 40, -105), scene)
+    // const camera = new FreeCamera("camera", new Vector3(-95, 40, -105), scene)
+    const camera = new BABYLON.ArcRotateCamera("Camera", 0.0, 1.3, 80, new BABYLON.Vector3(0, 10.0, 0), scene)
+
+    camera.attachControl(canvas, false)
     camera.setTarget(Vector3.Zero())
 
     // Light
-    const light1 = new HemisphericLight("light1", new Vector3(1, 0, 0), scene)
-    const light2 = new PointLight("light2", new Vector3(0, 60, 0), scene)
+    const light1 = new HemisphericLight("light1", new Vector3(0, 1, 0), scene)
+    light1.intensity = 0.8
+
+    const light2 = new BABYLON.DirectionalLight("DirectionalLight", new BABYLON.Vector3(60, -600, 200), scene)
+    light2.intensity = 1
 
     // Materials
     // - water
@@ -38,9 +44,42 @@ export const createScene = (engine, canvas, size) => {
     const waterCrust = addCrustSlice(scene, "water", 1.5, size + 15, size + 15, -4.75)
     waterCrust.parent = crust
     waterCrust.material = materialWater
-
+  
     crust.parent = group
+    
+    const shadowGenerator = new ShadowGenerator(1024, light2)
 
+    city._children[1]._children.forEach(element => {
+      shadowGenerator.getShadowMap().renderList.push(element)
+    })
+
+
+    shadowGenerator.forceBackFacesOnly = true
+    shadowGenerator.usePoissonSampling = true
+
+
+    const lensEffect = new BABYLON.LensRenderingPipeline('lens', {
+      edge_blur: 0,
+      chromatic_aberration: 0.0,
+      distortion: 0,
+      dof_focus_distance: 30,
+      dof_aperture: 3.0,			// set this very high for tilt-shift effect
+      grain_amount: 1.0,
+      dof_pentagon: false,
+      dof_threshold: 1.0,
+      dof_darken: 0
+    }, scene, 1.0, camera)
+
+    const postProcess = new BABYLON.FxaaPostProcess("fxaa", 1.0, camera)
+    const postProcess2 = new BABYLON.ImageProcessingPostProcess("processing", 1.0, camera)
+    postProcess2.vignetteWeight = 10
+    postProcess2.vignetteStretch = 2
+    postProcess2.vignetteColor = new BABYLON.Color4(1, 0, 0, 0)
+
+    // Pour passer en mode REEEEED
+    // postProcess2.vignetteEnabled = true
+
+  
     return scene
   }
 
