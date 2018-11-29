31bc74f11355fcf477ffa42ca12d86d233b98607
Naomi Hauret <naomi@troopers.email>
2018-11-29 11:59:19 +0100

    :hammer: wip: components

--- src/app/views/components/Event/index.js
+++ src/app/views/components/Event/index.js
@@ -1,7 +1,12 @@
 import { html } from 'lit-html'
 
-export const eventComponentFrom = (event) => html`
-  <li class='flex justify-between shadow-base'>
-    ${event.time} ${event.title}
+export const eventTemplate = (event) => html`
+  <li class='fs-sm flex justify-between shadow-base pl-20 pr-35 pv-15 mb-15'>
+    <span>
+      ${event.time}
+    </span>
+    <span>
+      ${event.title}
+    </span>
   </li>
 `
