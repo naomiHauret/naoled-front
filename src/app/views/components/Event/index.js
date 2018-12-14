6f1986ef8f5caa3ba83789f370101267380b1ebd
Naomi Hauret <naomi@troopers.email>
2018-12-14 13:59:07 +0100

    wip: wiring

--- src/app/views/components/Event/index.js
+++ src/app/views/components/Event/index.js
@@ -1,9 +1,9 @@
 import { h } from "hyperapp"
 
-export default (event) => {
+export default (data) => {
   return (
     <div class="bg-white radius-lg fs-sm flex wrap justify-between shadow-base pl-20 pr-35 pv-15 mb-15">
-      <span class="text-greyDark fw-medium"> {event.time} </span> <span class="text-grey"> {event.title} </span>
+      <span class="text-greyDark fw-medium"> {data.time} </span> <span class="text-grey"> {data.text} </span>
     </div>
   )
 }
