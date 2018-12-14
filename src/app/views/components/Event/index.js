a53c719917896d126b440f476b4da2f62c658752
Naomi Hauret <naomi@troopers.email>
2018-12-14 16:27:21 +0100

    :grimacing:

--- src/app/views/components/Event/index.js
+++ src/app/views/components/Event/index.js
@@ -10,20 +10,23 @@ function formatSeconds(milliseconds, format) {
   const o = {
     hh: getDoubleDigits(dateObj.getHours()),
     mm: getDoubleDigits(dateObj.getMinutes()),
-    ss: getDoubleDigits(dateObj.getSeconds())
+    ss: getDoubleDigits(dateObj.getSeconds()),
   }
 
-  const dilimeter = format.match(/[^\w]/)[0];
-  return format.split(dilimeter).map(function (f) {
-    return o[f]
-  }).join(dilimeter)
+  const dilimeter = format.match(/[^\w]/)[0]
+  return format
+    .split(dilimeter)
+    .map(function(f) {
+      return o[f]
+    })
+    .join(dilimeter)
 }
-
 
 export default (data) => {
   return (
     <div class="bg-white radius-lg fs-sm flex wrap justify-between shadow-base pl-20 pr-35 pv-15 mb-15">
-      <span class="text-greyDark fw-medium"> {formatSeconds(+data.data.time, "hh:mm")} </span> <span class="text-grey"> {data.data.text} </span>
+      <span class="text-greyDark fw-medium"> {formatSeconds(+data.data.time, "hh:mm")} </span>{" "}
+      <span class="text-grey"> {data.data.text} </span>
     </div>
   )
 }
